8f6f97f376237f12d619f17613840dca4fcbf21c
guswns1659 <guswns1659@gmail.com>
2020-03-16 22:22:04 +0900

    20.03.16) LeetCode - Count Primes 성공

---
+++ src/leetcodeEasy/CountPrimes.java
@@ -0,0 +1,29 @@
+package leetcodeEasy;
+
+import java.util.*;
+
+public class CountPrimes {
+    private List<Integer> primes = new ArrayList<>();
+
+    public int countPrimes(int n) {
+        primes.clear();
+        primes.add(2);
+        primes.add(3);
+        for (int each = 5; each < n; each+=2) {
+            boolean flag = true;
+            int i;
+            for (i = 0; primes.get(i) * primes.get(i) <= each; i++) {
+                if (each % primes.get(i) == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                primes.add(each);
+            }
+        }
+        if (n == 0 || n == 1 || n == 2) return 0;
+        if (n == 3) return 1;
+        return primes.size();
+    }
+}
