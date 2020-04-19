f8c73d1d2173bb921cedeca6666c3d90fb8657dc
hyunjun <guswns1659@gmail.com>
2020-04-19 11:48:02 +0900

    [Algorithm] 백준 - '잃어버린 괄호' 문제 풀이 코드
    - 감이 안 잡혀서 답안 보고 풀었음.

--- src/backjun/LostBracket.java
+++ src/backjun/LostBracket.java
@@ -3,27 +3,30 @@ package backjun;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class LostBracket {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] tokens = br.readLine().split("");
-        List<String> list = new ArrayList<>();
-        StringBuilder number = new StringBuilder();
+        String[] tokens = br.readLine().split("-");
 
-        for (String each : tokens) {
-            if (each.equals("+") || each.equals("-")) {
-                list.add(number.toString());
-                number = new StringBuilder();
-                list.add(each);
-            } else {
-                number.append(each);
-            }
+        int miniumResult = 0;
+
+        for (int count = 0; count < tokens.length; count++) {
+            int number = calculate(tokens[count]);
+
+            if (count == 0) number *= -1;
+            miniumResult -= number;
         }
-        list.add(number.toString());
 
-        list.forEach(System.out::println);
+        System.out.println(miniumResult);
+    }
+
+    private static int calculate(String token) {
+        String[] tokens = token.split("\\+");
+        int result = 0;
+        for (String each : tokens) {
+            result += Integer.parseInt(each);
+        }
+        return result;
     }
 }
