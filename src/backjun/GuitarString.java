7a9cfa02378cb9347727bb6b7a00bf3969a9184a
hyunjun <guswns1659@gmail.com>
2020-04-21 11:06:09 +0900

    [Algorithm] 백준 - '기타줄' 문제 풀이 코드
    - 패키지 최솟값과 낱개 최솟값으로 3가지 경우의 수를 계산.
      - 패키지 최솟값으로만 구하기.
      - 낱개 최솟값으로만 구하기.
      - 두 개 섞어서 하기.

--- src/backjun/GuitarString.java
+++ src/backjun/GuitarString.java
@@ -7,6 +7,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GuitarString {
+
+    private static final int A_PIECE_OF_PACKAGE = 6;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -26,8 +29,42 @@ public class GuitarString {
             pieces.add(Integer.parseInt(Abrand[1]));
         }
 
-        packages.forEach(System.out::println);
-        pieces.forEach(System.out::println);
+        int minPackage = packages.stream().mapToInt(price -> price).min().getAsInt();
+        int minPiece = pieces.stream().mapToInt(price -> price).min().getAsInt();
 
+        List<Integer> candidates = new ArrayList<>();
+
+        int quotient = need / A_PIECE_OF_PACKAGE;
+        int remainder = need % A_PIECE_OF_PACKAGE;
+
+        System.out.println("quotient:" + quotient);
+        System.out.println("remainder:" + remainder);
+
+        // 1. 패키지 가격 중 최솟값으로 가격을 구한다.
+        candidates.add(calculateWithMinPackages(quotient, remainder, minPackage));
+
+        // 2. 낱개 가격 중 최솟값으로 가격을 구한다.
+        candidates.add(need * minPiece);
+
+        // 3. 두 개를 섞어서 가격을 구한다.
+        candidates.add(calculateWithBoth(minPackage, minPiece, quotient, remainder));
+
+        int answer = candidates.stream().mapToInt(c -> c).min().getAsInt();
+        System.out.println(answer);
+    }
+
+    private static int calculateWithBoth(int minPackage, int minPiece, int quotient, int remainder) {
+        int minPrice = 0;
+        minPrice += (quotient * minPackage);
+        minPrice += (remainder * minPiece);
+        return minPrice;
+    }
+
+    private static int calculateWithMinPackages(int quotient, int remainder, int minPackage) {
+        if (remainder == 0) {
+            return quotient * minPackage;
+        } else {
+            return (quotient + 1) * minPackage;
+        }
     }
 }
