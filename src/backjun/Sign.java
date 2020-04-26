eb21f8d69210ba19251fe8f73f823d7192a6dd12
hyunjun <guswns1659@gmail.com>
2020-04-26 11:13:15 +0900

    [Algorithm] 백준 - '부등호' 문제 풀이 코드
    - DFS 활용이 필요한 문제.
    - 답안 보고 해결.

--- src/backjun/Sign.java
+++ src/backjun/Sign.java
@@ -1,18 +1,54 @@
 package backjun;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
+import java.util.ArrayList;
 
 public class Sign {
+    private static boolean[] used = new boolean[10];
+    private static int k;
+    private static String[] str;
+    private static ArrayList<String> result;
+
+    public static void solution(int cnt, int prior, String t) {
+        if (cnt == k) {
+            result.add(t);
+            used[prior] = false;
+            return;
+        }
+        for (int i = 0; i < 10; i++) {
+            if (used[i])
+                continue;
+            if (i == prior)
+                continue;
+            if (str[cnt].equals(">")) {
+                if (prior < i) {
+                    continue;
+                }
+            } else if (str[cnt].equals("<")) {
+                if (prior > i) {
+                    continue;
+                }
+            }
+            used[i] = true;
+            solution(cnt + 1, i, t + i);
+        }
+        used[prior] = false;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int repeat = Integer.parseInt(br.readLine());
-        System.out.println("repeat : " + repeat);
-
-        String[] signs = br.readLine().split(" ");
-        Arrays.stream(signs).forEach(System.out::println);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        k = Integer.parseInt(br.readLine());
+        str = br.readLine().split(" ");
+        result = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            used[i] = true;
+            solution(0, i, String.valueOf(i));
+        }
+        bw.write(result.get(result.size() - 1) + "\n");
+        bw.write(result.get(0) + "");
+        bw.flush();
+        bw.close();
 
     }
 }
