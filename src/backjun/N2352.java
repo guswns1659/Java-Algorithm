a63372aafc01b31f123a32a667e61bdf93e57536
hyunjun <guswns1659@gmail.com>
2020-04-29 11:17:58 +0900

    [Algorithm] 백준 - 2352번 문제 '반도체 설계' 풀이 코드
    - 입력값을 LinkedHashMap으로 변환해 순서를 보장.
      - ex) linkedHashMap{4=0, 3=0, 1=0, 2=0}

--- src/backjun/N2352.java
+++ src/backjun/N2352.java
@@ -3,32 +3,41 @@ package backjun;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class N2352 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int numberOfPorts = Integer.parseInt(br.readLine());
-        List<Integer> ports = Arrays.stream(br.readLine().split(" "))
+        List<Integer> portsList = Arrays.stream(br.readLine().split(" "))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
 
-        List<Integer> decreasingPorts = new ArrayList<>(ports);
+        Map<Integer, Integer> ports = new LinkedHashMap<>();
+        portsList.forEach(port -> ports.put(port, 0));
 
-        System.out.println("numberOfPorts : " + numberOfPorts);
+        System.out.println("linkedHashMap" + ports);
 
-        List<Long> possibles = new ArrayList<>();
+        Map<Integer, Integer> beforePorts = new LinkedHashMap<>();
 
-        for (Integer port : ports) {
-            long countOfBigger = decreasingPorts.stream().filter(each -> each > port).mapToInt(e -> e).count();
-            decreasingPorts.remove(port);
-            possibles.add(countOfBigger);
-        }
+//        List<Integer> decreasingPorts = new ArrayList<>(ports);
 
-        long answer = possibles.stream().mapToLong(e -> e).max().getAsLong();
-        System.out.println("answer : " + answer);
+
+//        List<Long> possibles = new ArrayList<>();
+//
+//        for (Integer port : ports) {
+//            long countOfBigger = decreasingPorts.stream()
+//                    .filter(each -> each > port).mapToInt(e -> e).count();
+//            decreasingPorts.remove(port);
+//            System.out.println("decreasingPorts : " + decreasingPorts);
+//            possibles.add(countOfBigger);
+//        }
+//
+//        long answer = possibles.stream().mapToLong(e -> e).max().getAsLong();
+//        System.out.println("answer : " + answer);
     }
 }
