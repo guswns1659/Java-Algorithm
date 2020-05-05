60bf1f774bd9d05ea99e8bf34df1cf64c3fa10b2
hyunjun <guswns1659@gmail.com>
2020-05-05 12:17:10 +0900

    [Algorithm] 백준 - 2352번 문제 '반도체 설계' 풀이 코드
    - 답안 참고해서 내가 직접 풀기.

--- src/backjun/N2352.java
+++ src/backjun/N2352.java
@@ -1,39 +1,43 @@
 package backjun;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class N2352 {
-    static int lis(int[] a) {
-        int[] tailTable = new int[a.length];
-
-        tailTable[0] = a[0]; // 초기값
+    static int lis(List<Integer> ports) {
+        int[] sequences = new int[ports.size()];
+        sequences[0] = ports.get(0);
         int length = 1;
 
-        for (int i = 1; i < a.length; i++) { //후보값이 처음값보다 작을 때
-            if (tailTable[0] > a[i]) {
-                tailTable[0] = a[i];
-            } else if (tailTable[length - 1] < a[i]) { //후보값이 마지막값보다 클 때
-                tailTable[length] = a[i];
-                length += 1;
-            } else { //그 외의 경우엔 적절한 위치에 교체한다.
-                int idx = Arrays.binarySearch(tailTable, 0, length, a[i]);
-                idx = (idx < 0) ? -idx - 1 : idx;
-                tailTable[idx] = a[i];
+        for (int index = 1; index < ports.size(); index++) {
+            int currentPort = ports.get(index);
+            if (sequences[0] > currentPort) {
+                sequences[0] = currentPort;
+            } else if (currentPort > sequences[length - 1]) {
+                sequences[length] = currentPort;
+                length++;
+            } else {
+                int location = Arrays.binarySearch(sequences, 0, length, currentPort);
+                location = location < 0 ? -location - 1 : location;
+                sequences[location] = currentPort;
             }
         }
-        System.out.println(Arrays.toString(tailTable));
+
         return length;
     }
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = scan.nextInt();
-        }
-        System.out.println(lis(a));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int countOfPorts = Integer.parseInt(br.readLine());
+        List<Integer> ports = Arrays.stream(br.readLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        System.out.println(lis(ports));
     }
 }
 
