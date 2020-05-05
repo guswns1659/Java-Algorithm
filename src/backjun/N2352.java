254e2fa4d097a310e0e44fbb63c468866dcb80b4
hyunjun <guswns1659@gmail.com>
2020-05-05 11:50:40 +0900

    [Algorithm] 백준 - 2352번 문제 '반도체 설계' 풀이 코드
    - LIS 문제.
    - 정답은 나오지만 배열의 원소가 겹치지 않아야 하는 조건에는 위배.
    - 길이는 최장 길이 수열과 같기 때문에 답을 통과.
    - Arrays.binarySearch() 사용 시 원소가 없으면 들어갈 자리가 음수로 나온다. 대신 들어갈 자리 시작은 1부터 시작.

--- src/backjun/N2352.java
+++ src/backjun/N2352.java
@@ -1,49 +1,39 @@
 package backjun;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Scanner;
 
 public class N2352 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int numberOfPorts = Integer.parseInt(br.readLine());
-        List<Integer> inputToList = Arrays.stream(br.readLine().split(" "))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+    static int lis(int[] a) {
+        int[] tailTable = new int[a.length];
 
-        Map<Integer, Integer> ports = new LinkedHashMap<>();
-        inputToList.forEach(port -> ports.put(port, 0));
+        tailTable[0] = a[0]; // 초기값
+        int length = 1;
 
-        System.out.println("linkedHashMap : " + ports);
-
-        Map<Integer, Integer> beforePorts = new LinkedHashMap<>();
-
-        int firstKey = inputToList.get(0);
-        int firstValue = ports.get(firstKey);
-        beforePorts.put(firstKey, ++firstValue);
-
-        System.out.println("beforePorts : " + beforePorts);
-
-        // 2번째 수부터
-        for (int index = 1; index < numberOfPorts; index++) {
-            int current = inputToList.get(index);
-            long countOfLowerThanCurrent = beforePorts.keySet().stream().filter(each -> each < current).count();
-            if (countOfLowerThanCurrent == 0) {
-                int currentValue = ports.get(current);
-                beforePorts.put(current, ++currentValue);
-            } else {
-                int maxBetweenLowers = beforePorts.keySet().stream().filter(each -> each < current).mapToInt(beforePorts::get).max().orElse(0);
-//                int currentValue = ports.get(current);
-                beforePorts.put(current, ++maxBetweenLowers);
+        for (int i = 1; i < a.length; i++) { //후보값이 처음값보다 작을 때
+            if (tailTable[0] > a[i]) {
+                tailTable[0] = a[i];
+            } else if (tailTable[length - 1] < a[i]) { //후보값이 마지막값보다 클 때
+                tailTable[length] = a[i];
+                length += 1;
+            } else { //그 외의 경우엔 적절한 위치에 교체한다.
+                int idx = Arrays.binarySearch(tailTable, 0, length, a[i]);
+                idx = (idx < 0) ? -idx - 1 : idx;
+                tailTable[idx] = a[i];
             }
         }
-        int answer = beforePorts.values().stream().mapToInt(each -> each).max().getAsInt();
-        System.out.println("answer : " + answer);
+        System.out.println(Arrays.toString(tailTable));
+        return length;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scan.nextInt();
+        }
+        System.out.println(lis(a));
     }
 }
+
