3f7974834ac8f9ee0135ad16e6753ad500a9a679
DevJun <guswns1659@gmail.com>
2020-08-14 17:20:46 +0900

    algorithm : N10989 알고리즘 풀이 : 계수수열
    - 정렬은 자주 등장하니 잘 반복주기로 하자

---
+++ src/backjun/N10989.java
@@ -0,0 +1,28 @@
+package backjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class N10989 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(br.readLine());
+        int[] array = new int[10001];
+        for (int loop = 0; loop < testCase; loop++) {
+            array[Integer.parseInt(br.readLine())]++;
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for (int index = 0; index < 10001; index++) {
+            if (array[index] != 0) {
+                for (int loop = 0; loop < array[index]; loop++) {
+                    result.append(index).append("\n");
+                }
+            }
+        }
+        System.out.print(result);
+    }
+}
