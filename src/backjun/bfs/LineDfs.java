afcb0bcc2ebe28e445c457ad3a98f4ed9fde28cb
DevJun <guswns1659@gmail.com>
2020-09-09 16:54:58 +0900

    Algorithm : 2019 라인 인턴 BFS 문제 풀이

---
+++ src/backjun/bfs/LineDfs.java
@@ -0,0 +1,87 @@
+package backjun.bfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LineDfs {
+
+    static int[] conyDist = new int[200001];
+    static int[] brownDist = new int[200001];
+    static int C, B;
+    static int second = -1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] NM = br.readLine().split(" ");
+
+        // 코니위치
+        C = Integer.parseInt(NM[0]);
+        // 브라운위치
+        B = Integer.parseInt(NM[1]);
+
+        Arrays.fill(conyDist, -1);
+        Arrays.fill(brownDist, -1);
+
+        System.out.println(test(C, B));
+    }
+    public static int test(int c, int b) {
+        // main으로 실행할 땐 지워야하는 조건들
+        C = c;
+        B = b;
+        Arrays.fill(conyDist, -1);
+        Arrays.fill(brownDist, -1);
+
+        Queue<Integer> q = new LinkedList<>();
+        conyDist[c] = 0;
+        brownDist[b] = 0;
+        q.offer(b);
+
+        /*
+         * 문제의 원인 q.poll()할 때마다 초가 증가하는데 q.poll()할 때 초가 증가하는 것이 아니라 트리의 깊이의 해당하는 노드가 끝났을 때 추가해야함.
+         */
+
+
+        while (!q.isEmpty()) {
+            int brownLocation = q.poll();
+            second++;
+
+            // 브라운이 해당 위치에 도달한 시간과 현재 시간이 같을 때만 코니 위치와 브라운의 위치가 같은지 판단한다.
+            if (brownDist[brownLocation] == second) {
+                int conyLocation = getConyLocation(second + 1);
+                // 이동한 코니 위치가 범위를 벗어나는 경우
+                if (conyLocation > 200000 || conyLocation < 0) return -1;
+                conyDist[conyLocation] = second;
+
+                // 위치가 같을 때 그 위치에 도달한 시간이 같은지 확인, 같다면 그 시간 리턴
+                if (conyLocation == brownLocation && conyDist[conyLocation] == brownDist[brownLocation]) {
+                    return second;
+                }
+            }
+
+            int brownLocation2;
+
+            for (int loop = 1; loop < 4; loop++) {
+                if (loop == 1 ) brownLocation2 = brownLocation - 1;
+                else if (loop == 2) brownLocation2 = brownLocation + 1;
+                else brownLocation2 = brownLocation * 2;
+
+                if (brownLocation2 > 200000 || brownLocation2 < 0) continue;
+                if (brownDist[brownLocation2] >= 0) continue;
+                brownDist[brownLocation2] = brownDist[brownLocation] + 1;
+                q.offer(brownLocation2);
+            }
+        }
+        return -1;
+    }
+
+    private static int getConyLocation(int num) {
+        if (num <= 1) {
+            return C;
+        }
+        return getConyLocation(num - 1) + num - 1;
+    }
+}
---
+++ test/backjun/bfs/LineDfsTest.java
@@ -0,0 +1,15 @@
+package backjun.bfs;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LineDfsTest {
+
+    @ParameterizedTest
+    @CsvSource({"11,2,5"})
+    void 상반기_LINE인턴_알고리즘을_테스트한다(int conyLocation, int brownLocation, int result) throws Exception {
+        assertThat(LineDfs.test(conyLocation, brownLocation)).isEqualTo(result);
+    }
+}
