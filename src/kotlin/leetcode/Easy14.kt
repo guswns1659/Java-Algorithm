155f837583cad6b46f32a815851ab330a06e7412
guswns1659 <guswns1659@gmail.com>
2024-09-11 11:10:00 +0900

    24.09.11 easy

--- src/kotlin/leetcode/Easy14.kt
+++ src/kotlin/leetcode/Easy14.kt
@@ -20,3 +20,48 @@ class Solution1 {
         return false
     }
 }
+
+// https://leetcode.com/problems/min-stack/
+class MinStack() {
+    private val stack1: MutableList<Int> = mutableListOf()
+    private val stack2: MutableList<Int> = mutableListOf()
+
+    fun push(`val`: Int) {
+        stack1.add(`val`)
+        if (stack2.isEmpty()) {
+            stack2.add(`val`)
+        } else {
+            if (stack2[stack2.size - 1] > `val`) {
+                stack2.add(`val`)
+            }
+        }
+    }
+
+    fun pop() {
+        val data = stack1.removeAt(stack1.size - 1)
+        if (data == stack2[stack2.size - 1]) {
+            stack2.removeAt(stack2.size -1)
+        }
+    }
+
+    fun top(): Int {
+        return stack1[stack1.size - 1]
+    }
+
+    fun getMin(): Int {
+        return stack2[stack2.size - 1]
+    }
+
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = MinStack()
+ * obj.push(`val`)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
+
+
+
