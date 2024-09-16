5b8344274de4c30b4a4b6d7f54b4c1473d2dbbb3
guswns1659 <guswns1659@gmail.com>
2024-09-17 00:17:59 +0900

    기존 java 파일을 gradle main 폴더로 넣기

--- src/test/java/backjun/N1236Test.java
+++ src/test/java/backjun/N1236Test.java
@@ -1,14 +1,10 @@
 package backjun;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import java.util.Arrays;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.in;
 
 public class N1236Test {
 
--- src/test/java/backjun/tree/N5639Test.java
+++ src/test/java/backjun/tree/N5639Test.java
@@ -1,9 +1,7 @@
 package backjun.tree;
 
-import org.junit.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
