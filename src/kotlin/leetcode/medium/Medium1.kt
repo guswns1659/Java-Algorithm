2a7c1f1c413e57f774da4f32dc7610889f3801cd
guswns1659 <guswns1659@gmail.com>
2024-09-28 12:35:11 +0900

    24.09.28 BinarySerach advanced. search index in a rotated array

--- src/kotlin/leetcode/medium/Medium1.kt
+++ src/kotlin/leetcode/medium/Medium1.kt
@@ -42,3 +42,36 @@ class Solution {
         private const val BLACK = 2
     }
 }
+
+// https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+class Solution1 {
+    fun search(nums: IntArray, target: Int): Int {
+        var l = 0
+        var r = nums.size - 1
+
+        while (l <= r) {
+            val mid = l + (r - l ) / 2
+
+            if (nums[mid] == target) return mid
+
+            // 왼쪽 배열이 오름차순인지 확인
+            if (nums[l] <= nums[mid]) {
+                // 목표 값이 이 정렬된 구간에 있는지 확인
+                if (target >= nums[l] && target < nums[mid]) {
+                    r = mid - 1
+                } else {
+                    l = mid + 1 // mid는 이미 7번 라인에서 체크했으니 +1 해준다
+                }
+            } else {
+                // 목표 값이 이 오른쪽 부분에 있는지 확인
+                if (target > nums[mid] && target <= nums[r]) {
+                    l = mid + 1
+                } else {
+                    r = mid - 1
+                }
+            }
+
+        }
+        return -1
+    }
+}
