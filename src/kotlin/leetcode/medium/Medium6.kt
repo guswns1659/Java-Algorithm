e811b27db183c99861cd083c420f0045930937e0
guswns1659 <guswns1659@gmail.com>
2024-10-01 11:24:29 +0900

    24.10.01 double binary search if there are several value in sorted array

--- src/kotlin/leetcode/medium/Medium6.kt
+++ src/kotlin/leetcode/medium/Medium6.kt
@@ -167,3 +167,60 @@ class Solution4 {
         return r - l - 1 // 리턴될 때는 이미 한번 벗어난 상태라 -1을 해준다.
     }
 }
+
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+class Solution5 {
+    fun searchRange(nums: IntArray, target: Int): IntArray {
+        /*
+        - 이진 탐색의 응용.
+        - 중복된 숫자가 여러개가 있을 때 처음과 끝 문자를 찾으려면 처음 문자찾는 이진탐색과 끝 문자 찾는 이진탐색이 각각 있어야 한다.
+            - 첫 문자 찾는 탐색에서는 해당값을 찾으면 right = mid - 1로 무조건 왼쪽만 찾도록 한다.
+            - 끝 문자 탐색에서는 해당값을 찾으면 left = mid + 1로 무조건 오른쪽만 찾도록 한다.
+         */
+        val first = findFirst(nums, target)
+        val last = findLast(nums, target)
+        return intArrayOf(first, last)
+    }
+
+    private fun findFirst(nums: IntArray, target: Int): Int {
+        var l = 0
+        var r = nums.size - 1
+        var result = -1
+
+        while(l <= r) {
+            val mid = l + (r - l) / 2
+            if (nums[mid] == target) {
+                result = mid
+                r = mid - 1
+            } else if (nums[mid] > target) {
+                r = mid - 1
+            } else {
+                l = mid + 1
+            }
+        }
+
+        return result
+    }
+
+    private fun findLast(nums: IntArray, target: Int): Int {
+        var l = 0
+        var r = nums.size - 1
+        var result = -1
+
+        while(l <= r) {
+            val mid = l + (r - l) / 2
+            if (nums[mid] == target) {
+                result = mid
+                l = mid + 1
+            } else if (nums[mid] > target) {
+                r = mid - 1
+            } else {
+                l = mid + 1
+            }
+        }
+
+        return result
+    }
+
+
+}
