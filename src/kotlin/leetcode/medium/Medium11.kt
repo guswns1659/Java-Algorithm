b0ad692205c70dc98f55cd690c1533c0a336af23
guswns1659 <guswns1659@gmail.com>
2024-10-02 22:03:42 +0900

    24.10.02 two ways for inorder traversal

---
+++ src/kotlin/leetcode/medium/Medium11.kt
@@ -0,0 +1,23 @@
+package leetcode.medium
+
+// https://leetcode.com/problems/binary-tree-inorder-traversal/
+fun inorderTraversal(root: TreeNode?): List<Int> {
+    val result = mutableListOf<Int>()
+    val stack = mutableListOf<TreeNode>()
+    var current: TreeNode? = root
+
+    while (current != null || stack.isNotEmpty()) {
+        // 왼쪽 노드로 계속 이동
+        while (current != null) {
+            stack.add(current)  // 스택에 노드를 추가
+            current = current.left
+        }
+        // 스택에서 노드를 꺼내서 처리
+        current = stack.removeAt(stack.size - 1)  // 스택에서 마지막 요소 제거
+        result.add(current.`val`)
+        // 오른쪽 노드로 이동
+        current = current.right
+    }
+
+    return result
+}
