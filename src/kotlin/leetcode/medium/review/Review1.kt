afd53bc9ef64ceb8dc1d9ce8291f9d7fe5e05813
guswns1659 <guswns1659@gmail.com>
2024-10-16 22:56:11 +0900

    24.10.16 BackTracking) review search-in-rotated-sorted-array

--- src/kotlin/leetcode/medium/review/Review1.kt
+++ src/kotlin/leetcode/medium/review/Review1.kt
@@ -68,6 +68,34 @@ fun b(n: Int, o: Int, c: Int, cur: String, list: MutableList<String>) {
     }
 }
 
+// https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+fun search(nums: IntArray, target: Int): Int {
+    var l = 0
+    var r = nums.size - 1
+
+    while (l <= r) {
+        val mid = l + (r - l) / 2
+
+        if (target == nums[mid]) return mid
+
+        if (nums[l] <= nums[mid]) {
+            if (target >= nums[l] && target < nums[mid]) {
+                r = mid - 1
+            } else {
+                l = mid + 1
+            }
+        } else {
+            if (target > nums[mid] && target <= nums[r]) {
+                l = mid + 1
+            } else {
+                r = mid - 1
+            }
+        }
+    }
+
+    return -1
+}
+
 
 fun main() {
     print(generateParenthesis(3))
